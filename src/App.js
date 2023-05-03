474b6e594617505f30e3d1426688a03f22d2b1e6
Benoit Alessandroni <benoit@happy-dev.fr>
2023-05-03 15:42:28 +0200

    initial: commit of react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import QRCode from "qrcode.react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.post(
+        "https://gaiax.vereign.com/ocm/connection/v1/invitation-url?alias=trust",
+        {
+          autoAcceptConnection: true,
+          alias: "string",
+          myLabel: "string",
+          myImageUrl: "string",
+        }
+      );
+      console.log(result.data);
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Invitation URL:</h2>
+      <p>{data.invitationURL}</p>
+      <h2>QR Code:</h2>
+      <QRCode value={data.invitationURL} />
     </div>
   );
 }
