394051f28df8c67b282657cf624d714214ee053b
Benoit Alessandroni <benoit@happy-dev.fr>
2023-05-03 15:54:24 +0200

    fix: invitationURL

--- src/App.js
+++ src/App.js
@@ -3,31 +3,46 @@ import axios from "axios";
 import QRCode from "qrcode.react";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.post(
-        "https://gaiax.vereign.com/ocm/connection/v1/invitation-url?alias=trust",
-        {
-          autoAcceptConnection: true,
-          alias: "string",
-          myLabel: "string",
-          myImageUrl: "string",
-        }
-      );
-      console.log(result.data);
-      setData(result.data);
+      try {
+        const result = await axios.post(
+          "https://gaiax.vereign.com/ocm/connection/v1/invitation-url?alias=trust",
+          {
+            autoAcceptConnection: true,
+            alias: "string",
+            myLabel: "string",
+            myImageUrl: "string",
+          }
+        );
+        console.log(result.data);
+        console.log(result.data.data);
+        console.log(result.data.data.invitationUrl);
+        setData(result.data.data);
+      } catch (error) {
+        setError(error);
+      }
     };
     fetchData();
   }, []);
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h2>Invitation URL:</h2>
-      <p>{data.invitationURL}</p>
+      <p>{data.invitationUrl}</p>
       <h2>QR Code:</h2>
-      <QRCode value={data.invitationURL} />
+      {data && data.invitationUrl && <QRCode value={data.invitationUrl} />}
     </div>
   );
 }
